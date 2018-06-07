7d6696bde98f033ce50d76874aefbe0925222444
Dagfinn Dybvig <dagfinn.dybvig@gmail.com>
2018-06-07 14:18:36 +0200

    Eclipse

--- Thing.java
+++ Thing.java
@@ -6,17 +6,18 @@ int nr;
 String name;
 String desc;
 int loc;
+boolean carry;
 
-
-public Thing(int nr, String na, String d, int  l){
+public Thing(int nr, String na, String d, int  l, boolean carry){
 	this.nr=nr;
 	this.name=na;
 	this.desc=d;
 	this.loc=l;
+	this.carry=carry;
 } //Thing()
 
 public void describe(){
-	System.out.println(this.name);
+	System.out.print(this.name); System.out.print(" - ");
 	System.out.println(this.desc);
 }  //describe()
 
